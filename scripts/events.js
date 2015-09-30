9aedcc5b85d9cb7269c506636a845e301621a2af
Ryan Tankersley <ryan.tankersley@interworks.com>
2015-09-29 21:22:36 -0500

    first commit

---
+++ scripts/events.js
@@ -0,0 +1,26 @@
+var events = [{
+	title: 'English Club',
+	start: '2015-09-14T18:30:00',
+	end: '2015-09-14T20:30:00',
+	description: 'hello'	
+},{
+	title: 'English Club',
+	start: '2015-09-21T18:30:00',
+	end: '2015-09-21T20:30:00',
+	description: 'hello'
+},{
+	title: 'English Club',
+	start: '2015-09-28T18:30:00',
+	end: '2015-09-28T20:30:00',
+	description: 'hello'
+},{
+	title: 'rawr',
+	start: '2015-09-18T12:00:00',
+	end: '2015-09-18T13:00:00',
+	description: 'hello'
+},{
+	title: 'FIL',
+	start: '2015-09-25T12:00:00',
+	end: '2015-09-25T13:00:00',
+	description: 'hello'
+}];
